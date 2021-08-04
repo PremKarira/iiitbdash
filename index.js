ff74c8b63a802cc636e99e8cd4e5bd3dc6c0a369
PremKarira <83239898+PremKarira@users.noreply.github.com>
2021-08-04 18:42:06 +0530

    changed footer and added console log

--- index.js
+++ index.js
@@ -62,19 +62,19 @@ client.on('message', message => {
 
     emb1.setTitle("ECE Time-Table")
     emb1.setColor('RANDOM')
-    emb1.setFooter("type -cs for more commands")
+    emb1.setFooter(message.author.tag, message.author.avatarURL);
     emb1.setTimestamp()
     emb1.setImage('https://images-ext-1.discordapp.net/external/07lpSzm4rSwjBupYrLo7xPPgfvOnLoPuFRePz6uzZQI/https/media.discordapp.net/attachments/816314803387367466/871396995191930891/Frame_10ECE_1.png?width=1259&height=669')
     
     emb2.setTitle("CSE Time-Table")
     emb2.setColor('RANDOM')
-    emb2.setFooter("type -cs for more commands")
+    emb2.setFooter(`Info requested by ${message.author.tag}`, message.author.displayAvatarURL)
     emb2.setTimestamp()
     emb2.setImage('https://images-ext-1.discordapp.net/external/1I7lpfmOrmrnfaMYlgDOzXVi0N2Sc1q8bt0wWzJwOBw/https/media.discordapp.net/attachments/816314803387367466/871396996488003604/Frame_10CSE_1.png?width=1259&height=669')
     
     emb3.setTitle("IT Time-Table")
     emb3.setColor('RANDOM')
-    emb3.setFooter("type -cs for more commands")
+    emb3.setFooter(client.user.id, client.user.displayAvatarURL);
     emb3.setTimestamp()
     emb3.setImage('https://images-ext-2.discordapp.net/external/gJyCVUXLVrk9ZtSK6RvnHjoJ40l-e7pQ3wOQc-zag6Y/https/media.discordapp.net/attachments/816314803387367466/872181270380167218/Frame_59IT.png?width=1426&height=670')
     
@@ -105,7 +105,9 @@ client.on('message', message => {
 
 client.on('message', message => {
   if (message.content.startsWith('::ping ') ) {
-    message.delete(1000);
+    message.delete({ timeout: 2000 })
+      .then(msg => console.log(`Deleted message from ${msg.author.username} after 5 seconds`))
+      .catch(console.error);
     if (!message.channel.name.includes("class-links")) message.channel.send('do this command only at <#868036126890426368>');
     else {
       let link = message.content.replace('::ping ', '')
@@ -128,29 +130,32 @@ client.on('message', message => {
         .catch(/*Your Error handling if the Message isn't returned, sent, etc.*/);
       message.channel.awaitMessages(m => m.author.id == message.author.id,
         {max: 1, time: 15000}).then(collected => {
+                console.log('${collected.first()} used')
                 if (collected.first().content.toLowerCase() == '1') {
                   message.channel.send('<@&824984224330416191>'); message.channel.send(emb)
-                  collected.first().delete();// client.destroy();
+                  // collected.first().delete();// client.destroy();
                 }
                 else if (collected.first().content.toLowerCase() == '2') {
                   message.channel.send('<@&824974978784690198>'); message.channel.send(emb)
-                  collected.first().delete();// client.destroy();
+                  // collected.first().delete();// client.destroy();
                 }
                 else if (collected.first().content.toLowerCase() == '3') {
                   message.channel.send('<@&824984226885402674>'); message.channel.send(emb)
-                  collected.first().delete();// client.destroy();
+                  // collected.first().delete();// client.destroy();
                 }
                 else if (collected.first().content.toLowerCase() == '4') {
                   message.channel.send('<@&824974978784690198> & <@&824984226885402674>'); message.channel.send(emb)
-                  collected.first().delete();// client.destroy();
+                  // collected.first().delete();// client.destroy();
                 }
                 else if (collected.first().content.toLowerCase() == '5') {
                   message.channel.send('<@&824984224330416191> & <@&824974978784690198> & <@&824984226885402674>'); message.channel.send(emb)
-                  collected.first().delete();// client.destroy();
+                  // collected.first().delete();// client.destroy();
                 }
                 else
                   message.reply('Operation canceled.');
+
                 collected.first().delete();
+                console.log('${collected.first()} deleted')
         }).catch(() => {
                 message.reply('No answer after 30 seconds, operation canceled.');
         });
