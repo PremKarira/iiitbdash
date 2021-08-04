3dae472ad0b209c732fe1734740b7109e4f75054
PremKarira <83239898+PremKarira@users.noreply.github.com>
2021-08-04 18:51:28 +0530

    changed footer of embeds and fixed cd

--- index.js
+++ index.js
@@ -51,6 +51,7 @@ client.on('message', message => {
 
 client.on('message', message => {
   if (message.content === '-tt') {
+    const UserPFP = message.member.avatarURL();
     message.channel.send('Wait for my reply')
       .then(msg => {
         msg.delete({ timeout: 15000 /*time unitl delete in milliseconds*/});
@@ -62,19 +63,19 @@ client.on('message', message => {
 
     emb1.setTitle("ECE Time-Table")
     emb1.setColor('RANDOM')
-    emb1.setFooter(message.author.tag, message.author.avatarURL);
+    emb1.setFooter(`Info requested by ${message.author.tag}`, UserPFP)
     emb1.setTimestamp()
     emb1.setImage('https://images-ext-1.discordapp.net/external/07lpSzm4rSwjBupYrLo7xPPgfvOnLoPuFRePz6uzZQI/https/media.discordapp.net/attachments/816314803387367466/871396995191930891/Frame_10ECE_1.png?width=1259&height=669')
     
     emb2.setTitle("CSE Time-Table")
     emb2.setColor('RANDOM')
-    emb2.setFooter(`Info requested by ${message.author.tag}`, message.author.displayAvatarURL)
+    emb2.setFooter(`Info requested by ${message.author.tag}`, UserPFP)
     emb2.setTimestamp()
     emb2.setImage('https://images-ext-1.discordapp.net/external/1I7lpfmOrmrnfaMYlgDOzXVi0N2Sc1q8bt0wWzJwOBw/https/media.discordapp.net/attachments/816314803387367466/871396996488003604/Frame_10CSE_1.png?width=1259&height=669')
     
     emb3.setTitle("IT Time-Table")
     emb3.setColor('RANDOM')
-    emb3.setFooter(client.user.id, client.user.displayAvatarURL);
+    emb3.setFooter(`Info requested by ${message.author.tag}`, UserPFP)
     emb3.setTimestamp()
     emb3.setImage('https://images-ext-2.discordapp.net/external/gJyCVUXLVrk9ZtSK6RvnHjoJ40l-e7pQ3wOQc-zag6Y/https/media.discordapp.net/attachments/816314803387367466/872181270380167218/Frame_59IT.png?width=1426&height=670')
     
@@ -106,9 +107,15 @@ client.on('message', message => {
 client.on('message', message => {
   if (message.content.startsWith('::ping ') ) {
     message.delete({ timeout: 2000 })
-      .then(msg => console.log(`Deleted message from ${msg.author.username} after 5 seconds`))
+      .then(msg => console.log(`Deleted message from ${msg.author.username} after 2 seconds`))
       .catch(console.error);
-    if (!message.channel.name.includes("class-links")) message.channel.send('do this command only at <#868036126890426368>');
+    if (!message.channel.name.includes("class-links")) {
+      message.channel.send('do this command only at <#868036126890426368>')
+        .then(msg => {
+          msg.delete({ timeout: 5000 /*time unitl delete in milliseconds*/});
+        })
+        .catch(/*Your Error handling if the Message isn't returned, sent, etc.*/);
+    }
     else {
       let link = message.content.replace('::ping ', '')
       let emb = new Discord.MessageEmbed();
@@ -133,31 +140,31 @@ client.on('message', message => {
                 console.log('${collected.first()} used')
                 if (collected.first().content.toLowerCase() == '1') {
                   message.channel.send('<@&824984224330416191>'); message.channel.send(emb)
-                  // collected.first().delete();// client.destroy();
                 }
                 else if (collected.first().content.toLowerCase() == '2') {
                   message.channel.send('<@&824974978784690198>'); message.channel.send(emb)
-                  // collected.first().delete();// client.destroy();
                 }
                 else if (collected.first().content.toLowerCase() == '3') {
                   message.channel.send('<@&824984226885402674>'); message.channel.send(emb)
-                  // collected.first().delete();// client.destroy();
                 }
                 else if (collected.first().content.toLowerCase() == '4') {
                   message.channel.send('<@&824974978784690198> & <@&824984226885402674>'); message.channel.send(emb)
-                  // collected.first().delete();// client.destroy();
                 }
                 else if (collected.first().content.toLowerCase() == '5') {
                   message.channel.send('<@&824984224330416191> & <@&824974978784690198> & <@&824984226885402674>'); message.channel.send(emb)
-                  // collected.first().delete();// client.destroy();
                 }
                 else
-                  message.reply('Operation canceled.');
-
+                  message.reply('Operation canceled.')
+                    .then(msg => {
+                      msg.delete({ timeout: 5000 });
+                    })
                 collected.first().delete();
                 console.log('${collected.first()} deleted')
         }).catch(() => {
-                message.reply('No answer after 30 seconds, operation canceled.');
+                message.reply('No answer after 30 seconds, operation canceled.')
+                  .then(msg => {
+                    msg.delete({ timeout: 5000 });
+                  })
         });
     }
   }
